43e2b3fda174d1bca87399a48fb46b3d76482d52
Ashley Sands <ashsands1@amstrike.com>
2020-06-07 04:31:42 +0100

    added static load content

--- terminal/Includes/js/commands/command.js
+++ terminal/Includes/js/commands/command.js
@@ -25,4 +25,23 @@ class Command
         return this.validCommands.includes( comm.toLowerCase(), 0 );
     }
 
+    static LoadContent( url, responceElem )
+    {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                responceElem.innerHTML = this.responseText;
+            }
+            else if ( this.status >= 300)
+            {
+                responceElem.innerHTML = "Error"
+            }
+        };
+        
+        request.open("GET", url, true);
+        request.send();
+    }
+
 }
