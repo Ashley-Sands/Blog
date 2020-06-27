766bc9d5b559fa824764fa1941169ee5e3c458e7
Ashley Sands <ashsands1@amstrike.com>
2020-06-27 22:05:55 +0100

    added fetch head

--- terminal/includes/js/common.js
+++ terminal/includes/js/common.js
@@ -25,4 +25,30 @@ class Common
         request.send();
     }
 
+    static FetchHeader(url, header, responceElem, responceStr="{responce}")
+    {
+        /**
+         * @param responceStr: the string that the responce should be formated into.
+         *                     the string must contain '{responce}' where the responce
+         *                     will be printed
+         */
+
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                responceElem.innerHTML = responceStr.replace( /{responce}/g, this.getResponseHeader(header) );
+            }
+            else if ( this.status >= 300)
+            {
+                responceElem.innerHTML = `Error: ${this.status}`
+            }
+        };
+
+        request.open("HEAD", url, true);
+        request.send();
+
+    }
+
 }
