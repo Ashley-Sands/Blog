f9d4b2fd713bc9b967a7553dc3fedd7ea0dea2c6
Ashley Sands <ashsands1@amstrike.com>
2020-07-04 23:29:47 +0100

    added override outputs and cleaned up
    plue added some comments for futhure me :)

--- includes/js/mdParser.js
+++ includes/js/mdParser.js
@@ -9,22 +9,63 @@ class MarkDownParse{
         "ADDITIVE": 1
     }
 
-    constructor(){
+    constructor( overrideOutputs={} ){
 
+        // define default html elements.
+        // this alows use to replace any that are supplied by params
+        // while insuring that all required are still present.
+        var outputs = { 
+            header: {
+                "#": "<h1>{v0}</h1>",
+                "##": "<h2>{v0}</h2>",
+                "###": "<h3>{v0}</h3>",
+                "####": "<h4>{v0}</h4>",
+                "#####": "<h5>{v0}</h5>",
+                "######": "<h6>{v0}</h6>"
+            },
+            paragraph: {
+                "undefined": "{v0}",
+                "  ": "{v0}<br />",
+                "$complete": "<p>{v}</p>" // additive must contatin an output var $compleat, for the accumulated text to be outputed into. Notice {v} rather than {v0}
+            },
+            boldItalic: {
+                "*": "<i>{v0}</i>",
+                "**": "<b>{v0}</b>"
+            },
+            linksImages: {
+                "undefined": "<a href='{v1}'>{v0}</a>",
+                "!": "<img src='{v1}' alt='{v0}' />"
+            }
+
+        };
+
+        // update the output.
+        var overrideOutputKeys = Object.keys( overrideOutputs ) 
+
+        for ( var ok = 0; ok < overrideOutputKeys.length; ++ek )
+        {
+            var outputKey = overrideOutputKeys[ok];
+            var overrideElementKeys = Object.keys[outputKey];
+
+            for ( var ek = 0; ek < overrideElementKeys.length; ++ek )
+            {
+                var elementKey = overrideElementKeys[ek];
+                outputs[outputKey][elementKey] = overrideOutputs[outputKey][elementKey];
+            }
+
+        }
+
+        // define regexParseObjs
+        // The line regex parses line by line, to create the basic output with headers, paragraphs and line breaks, 
+        // then the after regex is executed untill there are no more regex matches.
+        // (after regex is preformed on the whole output in one go, rather than line by line.) 
         this.lineRegex = {
             header: {
                 regex: /(^##{0,5}) (.+)/,            
                 outKeyCapGroups: [1],      //this list id must match the values list id
                 valueCapGroups: [[2]],
                 lineMode: MarkDownParse.lineMode.NORMAL,
-                output: {
-                    "#": "<h1>{v0}</h1>",
-                    "##": "<h2>{v0}</h2>",
-                    "###": "<h3>{v0}</h3>",
-                    "####": "<h4>{v0}</h4>",
-                    "#####": "<h5>{v0}</h5>",
-                    "######": "<h6>{v0}</h6>",
-                }
+                output: outputs.header
                 
             },
             paragraph: {
@@ -33,33 +74,23 @@ class MarkDownParse{
                 outKeyCapGroups: [2, 2],      //this list id must match the values list id
                 valueCapGroups: [[1], [0]],
                 lineMode: MarkDownParse.lineMode.ADDITIVE,
-                output: {  // additive must contatin an output var $compleat, for the text to be outputed into
-                    "undefined": "{v0}",
-                    "  ": "{v0}<br />",
-                    "$complete": "<p>{v}</p>"
-                }
+                output: outputs.paragraph
             }
 
         };
 
         this.afterRegex = {
             boldItalic: {
-                regex: /((\*{2})([!-)+-~]+)\*{2})|((\*{1})([!-)+-~]+)\*{1})/,
+                regex: /((\*{2})([!-)+-~]+)\*{2})|((\*{1})([!-)+-~]+)\*{1})/, 
                 outKeyCapGroups: [2, 5],      // this ant right...
                 valueCapGroups: [[3], [6]],
-                output: {
-                    "*": "<i>{v0}</i>",
-                    "**": "<b>{v0}</b>"
-                }
+                output: outputs.boldItalic
             },
             linksImages: {
                 regex: /(!)*\[([ -Z\\^-~]*)\]\(([ -'*-~]*)\)/,
                 outKeyCapGroups: [1],      
                 valueCapGroups: [[2, 3]],
-                output: {
-                    "undefined": "<a href='{v1}'>{v0}</a>",
-                    "!": "<img src='{v1}' alt='{v0}' />"
-                }
+                output: outputs.linksImages
             }
         };
 
@@ -72,27 +103,30 @@ class MarkDownParse{
         var additiveOutputString = null;
         var output = "";
         
-        // 
+         
         for ( var i = 0; i < lines.length; ++i)
         {
-            // find if header.
+            // parse line as a header.
             var header = this._parse( this.lineRegex.header, lines[i] );
 
             if ( header != null )
             {
-                if ( additiveString != null)
+                // if we're been in additive mode, add the additive text to the output
+                // befor adding the header
+                if ( additiveString != null)    
                 {
                     output += additiveOutputString.replace(/{v}/, additiveString);
-                    additiveString = null;
+                    additiveString = null;  // disable additive mode.
                 }
 
                 output += header;
                 continue;
             }
 
+            // parse paragraphs and line breaks.
             var paragraph = this._parse( this.lineRegex.paragraph, lines[i] );        
             
-            if ( additiveString == null )
+            if ( additiveString == null )   // enable additive mode.
             {
                 additiveString = paragraph == null ? lines[i] : paragraph;
                 additiveOutputString = this.lineRegex.paragraph.output["$complete"];
@@ -104,20 +138,20 @@ class MarkDownParse{
 
         }
 
-        if ( additiveString != null)
+        if ( additiveString != null)    // if we where in additive mode, add it to the output.
         {
             output += additiveOutputString.replace(/{v}/, additiveString);
             additiveString = null;
         }
 
+        // parse all of the affter regex
         var values = Object.values(this.afterRegex);
         
-
         for ( var i = 0; i < values.length; ++i)
         {
             var temp = this._parse( values[i], output, true )
 
-            if ( temp != null )
+            if ( temp != null ) // if nothing was parsed insert the original string.
                 output = temp;
         }
 
@@ -128,13 +162,17 @@ class MarkDownParse{
     _parse(regexParseObj, string, update=false)
     {
         /**
-         * @returns: null if no match.
+         * @returns: null if no match, otherwise parsed string
          */
 
-        
-        var output = `${string}\n`;
+        // we must add the newline back to the end of string
+        // so we can detect line breaks '/(  )\n/'
+        var output = `${string}`+ !update ?? '\n' ; 
         var parsed = false;
 
+        // parse the string at least once.
+        // when not in update, the string is only parsed once (line by line mode),
+        // otherwise parse untill theres no regex matches remaining.
         do{
 
             var regGroups = regexParseObj.regex.exec(output);
@@ -146,24 +184,23 @@ class MarkDownParse{
 
                 for ( var j = 0; j < regexParseObj.outKeyCapGroups.length; j++ )
                 {
-
+                    // find if any of the 'capture group output keys' are in the output object
                     if ( regGroups[ regexParseObj.outKeyCapGroups[j] ] in regexParseObj.output )
                     {
                         var tempOutput = regexParseObj.output[ regGroups[ regexParseObj.outKeyCapGroups[j] ] ];
-                        // parsh into html
+
+                        // parsh all of the ca'pture group output values' into 
+                        // the output @ 'capture group output keys' html element.
                         for( var k = 0; k < regexParseObj.valueCapGroups[j].length; k++)
                         {
                             tempOutput =  tempOutput.replace(`{v${k}}`, regGroups[ regexParseObj.valueCapGroups[j][k] ]);  
                         }
                         
-                        if ( update ){
-
-                            output = output.replace(regGroups[0], tempOutput);
-
-                        }else
-                        {
+                        if ( update )
+                            output = output.replace(regGroups[0], tempOutput);  // replace the (whole) match with the output value.
+                        else
                             output = tempOutput;
-                        }
+                        
 
                         parsed = true;
 
@@ -180,7 +217,3 @@ class MarkDownParse{
     }
 
 }
-
-mdp = new MarkDownParse();
-
-/*MarkDownParse.parse("# dsagfda");*/
